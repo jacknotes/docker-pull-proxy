b21cb685a4851832bb13cbbb3a953b43bae44cbe
JackLi <l>
2024-06-12 10:26:07 +0800

    update

--- pull.sh
+++ pull.sh
@@ -23,9 +23,3 @@ cat trigger.txt | grep -Ev '^#|^$' | awk '{print "docker tag "$1 " " $2} ' | sh
 # docker push
 cat trigger.txt | grep -Ev '^#|^$' | awk '{print "docker push " $2} '
 cat trigger.txt | grep -Ev '^#|^$' | awk '{print "docker push " $2} ' | sh
-
-<<<<<<< HEAD
-
-
-=======
->>>>>>> dev
